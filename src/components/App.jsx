749914ec5a08a3ff2a720387feee64ce07e1428e
NinoTraverso <njt98git@proton.me>
2024-10-03 23:02:56 +0200

    startin new portfolio project

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div id="appContainer">
+      <title>CometOwl</title>
+      <meta
+        name="description"
+        content="CometOwl is your ultimate resource for astronomy articles, space exploration, cosmology insights, and astrophysics. Discover the mysteries of the universe with our expert content."
+      />
+      <meta
+        name="keywords"
+        content="astronomy, cometowl, CometOwl, space, cosmology, science, astrophysics, articles, exoplanets, planetary sciences, Astronomy Website"
+      />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
