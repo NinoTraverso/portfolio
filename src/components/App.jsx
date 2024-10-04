105d3585c5a05208c3c15ebc52ffb2a983ae66dd
NinoTraverso <njt98git@proton.me>
2024-10-04 06:26:07 +0200

    fixed background issue with components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 
+import Profile from "./Profile";
+
 function App() {
   return (
     <div id="appContainer">
       <title>NinoTraversoPortfolio</title>
       <meta name="description" content="Nino Traverso Digital Portfolio." />
       <meta name="keywords" content="portfolio, web design, web development" />
+
+      <div id="appContainer">
+        <Profile />
+      </div>
     </div>
   );
 }
