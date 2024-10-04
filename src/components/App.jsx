1ee6c085a52b659577f5b8ee5a7139adced963b5
NinoTraverso <njt98git@proton.me>
2024-10-04 06:05:12 +0200

    starting general setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,9 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div id="appContainer">
-      <title>CometOwl</title>
-      <meta
-        name="description"
-        content="CometOwl is your ultimate resource for astronomy articles, space exploration, cosmology insights, and astrophysics. Discover the mysteries of the universe with our expert content."
-      />
-      <meta
-        name="keywords"
-        content="astronomy, cometowl, CometOwl, space, cosmology, science, astrophysics, articles, exoplanets, planetary sciences, Astronomy Website"
-      />
-      <h1>Hello</h1>
+      <title>NinoTraversoPortfolio</title>
+      <meta name="description" content="Nino Traverso Digital Portfolio." />
+      <meta name="keywords" content="portfolio, web design, web development" />
     </div>
   );
 }
