fabd268d5554ca910682e2a9e6ca205748af1afb
NinoTraverso <njt98git@proton.me>
2024-10-04 07:12:52 +0200

    updated logo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,8 +3,14 @@ import React from "react";
 function Profile() {
   return (
     <div>
-      <div className="logoContainer">
-        <img src="./assets/logo.png" alt="" />
+      <div className="d-flex flex-row jutify-content-start align-items-end mt-4 ms-4">
+        <div className="logoContainer ">
+          <img src="./assets/logo.png" alt="logoImage" />
+        </div>
+        <div className="mx-3">
+          <h1>Nino J. Traverso</h1>
+          <h1>Web Developer & Designer</h1>
+        </div>
       </div>
       <h1 id="profile">Profile</h1>
     </div>
