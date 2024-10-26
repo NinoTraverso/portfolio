7a09ef1acd132c59c984073064728fa3c8b0e9f0
NinoTraverso <njt98git@proton.me>
2024-10-26 13:13:33 +0200

    adjusted project technology info

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,7 @@ const projects = [
     img: "../assets/projectsImages/cometOwl.jpg",
     description:
       "After studying Astronomy for 4 years at university and successfully completed an Astronomy Research Internship I decided to create a website to blend my Web Development skills with my Astronomy knowledge. From information on our Solar System, a multi-functional glossary, insighftul and up-to-date articles (which I personally write with no use of AI) and an exoplanets listing using NASA's API with an ID search tool I plan to slowly add more features and perfect existing ones. With time, as it is my hobby, will be to slowly implement new features for a better user experience who, like me, has a passion for Astronomy.",
-    technology: "HTML, CSS, JS, React",
+    technology: "HTML, CSS, JavaScript, React",
     projectLink: "https://www.cometowl.com/",
   },
 
@@ -15,7 +15,7 @@ const projects = [
     img: "../assets/projectsImages/cutzNtrimz.jpg",
     description:
       "A website for a barber shop with four sections: home , services and produts showing the different services offered by the barber as well as the variety of products he sells in his shop, and info. There are also dedicated pages for Icelandic and Polish.",
-    technology: "HTML, CSS, JS, React",
+    technology: "HTML, CSS, JavaScript, React",
     projectLink: "https://cutz-n-trimz.vercel.app/",
   },
   {
@@ -24,7 +24,7 @@ const projects = [
     img: "../assets/projectsImages/observaSky.jpg",
     description:
       "A platform designed for for an observatory where stargazers can keep track of upcoiming astronomy evenets, book astronomy lectures, read articles and explore the astronomy glossary.Astronomers maintain all the contents of the website, including users.",
-    technology: "HTML, CSS, JS, C#, .NET, SQL, SSMS",
+    technology: "HTML, CSS, JavaScript, C#, .NET, SQL, SSMS",
     projectLink: "https://github.com/NinoTraverso/observaSky",
   },
 ];
